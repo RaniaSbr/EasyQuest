65656376b20c5082bfd7c5e899e4d910592ec0ea
kenza <lk_bouaziz@esi.dz>
2023-12-11 18:27:55 +0100

    1st commit

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import ProfileComponent from "../Components/ProfileComponent";
 function Profile(params) {
   return (
     <div className="profile">
       <Navbar></Navbar>
+      <ProfileComponent></ProfileComponent>
     </div>
   );
 }
