dcd8382b9ddf61292a6247f38cc4655528f5acb5
Si Saber Rania <lr_sisaber@esi.dz>
2023-12-11 13:32:34 +0100

    rania's branch

---
+++ my-app/src/pages/Profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+function Profile(params) {
+  return (
+    <div className="profile">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+export default Profile;
