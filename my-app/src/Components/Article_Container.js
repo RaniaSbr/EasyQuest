40da1ca700e0c8deeca50dd19277db6bb2ee42b5
MahrazAbdelrahmen <la_mahraz@esi.dz>
2023-12-24 19:31:36 +0100

    fixed problem in article container where the text was not hyphenated.

--- my-app/src/Components/Article_Container.js
+++ my-app/src/Components/Article_Container.js
@@ -1,45 +1,69 @@
-import React from 'react';
-import '../Styles/Article_Container.css';
+import React from 'react'
+import '../Styles/Article_Container.css'
+import Hyphenated from 'react-hyphen';
 
 const ArticleContainer = ({ articleData }) => {
-  const { date, title, authors, institutions, url } = articleData;
+  const { date, title, authors, institutions, url } = articleData
   return (
-    <div className=" mx-4 md:mx-8 lg:mx-16 xl:mx-24 mt-32 p-5 rounded-lg border-2 border-lightStartD bg-grey">
+    <Hyphenated>
+    <div className='overflow-wrap break-word mx-4 md:mx-8 lg:mx-16 xl:mx-24 mt-32 p-5 rounded-lg border-2 border-lightStartD bg-grey'>
       <div className='mod-article-row'>
         <time className='font-Montserrat'>{date}</time>
         <div className='mod-article-dropdown'>
-          <span><img src='./Assets/td.png' alt="Icon" /></span>
+          <span>
+            <img src='./Assets/td.png' alt='Icon' />
+          </span>
           <div className='mod-article-dropdown-content'>
-            <div className='mod_article_text'><a href='#'>EDIT</a></div>
-            <div className='mod_article_text'><a href='#' className="text-red-500">Delete</a></div>
-          </div>  
+            <div className='mod_article_text'>
+              <a href='#'>EDIT</a>
+            </div>
+            <div className='mod_article_text'>
+              <a href='#' className='text-red-500'>
+                Delete
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="mt-5 ml-5 font-Montserrat text-green font-bold text-2xl">{title}</div>
-
-      <div className="mt-5 ml-5 font-Montserrat font-bold text-xl">
+        <p className=' hyphens-auto mt-5 ml-5 font-Montserrat text-green font-bold text-2xl '>
+          {title}
+        </p>
+      <div className='mt-5 ml-5 font-Montserrat font-bold text-xl'>
         {/* Authors */}
         {authors.map((author, index) => (
           <React.Fragment key={index}>
-            <a href="#" className="underline decoration-sky-500 font-Montserrat">{author}</a>
+            <a
+              href='#'
+              className='underline decoration-sky-500 font-Montserrat'
+            >
+              {author}
+            </a>
             {index < authors.length - 1 && ' | '}
           </React.Fragment>
         ))}
       </div>
 
-      <div className="mt-5 ml-5 font-Montserrat font-bold text-xl">
+      <div className='mt-5 ml-5 font-Montserrat font-bold text-xl'>
         {/* Institutions */}
         {institutions.map((institution, index) => (
-          <div className='font-Montserrat' key={index}>{institution} |</div>
+          <div className='font-Montserrat' key={index}>
+            {institution} |
+          </div>
         ))}
       </div>
 
-      <div className="mt-5 ml-5 font-Montserrat font-bold text-xl">URL :
-        <a href={url} className=" text-clip mt-5 ml-5 font-Montserrat font-bold italic text-xl text-green underline decoration-lightStartD">{url}</a>
+      <div className='hyphens-auto mt-5 ml-5 font-Montserrat font-bold text-xl'>
+        URL :
+        <a
+          href={url}
+          className='mt-5 ml-5 font-Montserrat font-bold italic text-xl text-green underline decoration-lightStartD'
+        >
+          {url}
+        </a>
       </div>
     </div>
-  );
-};
+    </Hyphenated>
+  )
+}
 
-export default ArticleContainer;
+export default ArticleContainer
