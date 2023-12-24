f1140a78d6b3bbfbc78d1b1f7a27a04d026f6b81
MahrazAbdelrahmen <la_mahraz@esi.dz>
2023-12-24 17:54:42 +0100

    ARTICLE CONTAINER+ANIMATED_TEXT

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -31,3 +32,19 @@ module.exports = {
   },
   plugins: [],
 }
+=======
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      display: ["group-hover", "responsive", "hover"],
+    },
+  },
+  plugins: [],
+};
+>>>>>>> origin/MAHRAZABDELRAHMEN
