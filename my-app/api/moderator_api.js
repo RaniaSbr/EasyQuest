1362300a409b94f092ec2e0e20341008f6a5e052
MahrazAbdelrahmen <la_mahraz@esi.dz>
2023-12-27 15:48:21 +0100

    deleted duplicat api/

--- my-app/api/moderator_api.js
+++ my-app/api/moderator_api.js
@@ -1,15 +1,47 @@
-
-import axios from 'axios';
-
-const BASE_URL = 'http://localhost:8000/';
+import axios from 'axios'
 
 
-export const fetchModerators = async () => {
-  try {
-    const response = await axios.get(`${BASE_URL}moderators/mods`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching moderators from database:', error);
-    throw error; 
+// needs comments + handling errors shall be better.
+class ModeratorAPI {
+  static handleCreate = (
+    url,
+    moderatorData,
+    successCallback,
+    errorCallback
+  ) => {
+    axios
+      .post(url, moderatorData)
+      .then(response => {
+        successCallback(response)
+      })
+      .catch(error => {
+        errorCallback(error)
+      })
   }
-};
+  static handleDelete = (id, url) => {
+    axios
+      .delete(`${url}/${id}`)
+      .then(response => {
+        console.log('Moderator deleted successfully:', response.data.message)
+        fetchModerators()
+      })
+      .catch(error => console.error(error))
+  }
+  static fetchModerators = (
+    url,
+    setModerators,
+    successCallback,
+    errorCallback
+  ) => {
+    axios
+      .get(url)
+      .then(response => {
+        setModerators(response.data)
+        successCallback(response)
+      })
+      .catch(error => errorCallback(error))
+  }
+}
+
+
+export default ModeratorAPI;
