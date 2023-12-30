b2935292c56471141c44d67d2057f66ec7edb2d4
yasmiinie <ly_zaidi@esi.dz>
2023-12-30 17:15:02 +0100

    search front back 70%

--- my-app/src/Components/SearchResults.js
+++ my-app/src/Components/SearchResults.js
@@ -1,28 +1,15 @@
-// frontend/src/SearchResults.js
 
-import React, { useState } from "react";
-
-const SearchResults = () => {
-  const [results, setResults] = useState([]);
-
-  const handleSearch = async () => {
-    const response = await fetch("/api/search/?q=your-search-query");
-    const data = await response.json();
-    setResults(data.results);
-  };
-
+import React from "react";
+import "../Styles/Search_result.css";
+import Navbar from "../Components/Navbar";
+import SearchResults from "../Components/SearchResults.js";
+function Search_result(params) {
   return (
-    <div>
-      <button onClick={handleSearch}>Search</button>
-      <ul>
-        {results.map((result, index) => (
-          <li key={index}>
-            {result.username} - {result.email}
-          </li>
-        ))}
-      </ul>
+    <div className="result">
+      <Navbar></Navbar>
+      <SearchResults></SearchResults>
     </div>
   );
-};
+}
+export default Search_result;
 
-export default SearchResults;
